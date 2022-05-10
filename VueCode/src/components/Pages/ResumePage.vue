ec7af2cb358436887b24ad85dd35245fbea31560
Aaron Hobgood <hobogod3@gmail.com>
2022-05-10 01:11:18 -0400

    chore: setup font-awesome and added svg. Init setup of Resume download

--- VueCode/src/components/Pages/ResumePage.vue
+++ VueCode/src/components/Pages/ResumePage.vue
@@ -2,6 +2,12 @@
     <div>
         <p>RESUME</p>
         <!-- src="../../assets/2022_Resume.pdf" -->
+        <img alt="Vue logo" 
+          class=""
+          style="width:30px;"
+          src="../../assets/file-arrow-down-solid.svg">
+        
+        <a target="_blank" href="~2022_Resume.pdf">Resume</a>
     </div>
 </template>
 
